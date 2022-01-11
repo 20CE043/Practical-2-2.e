75ea152e00698324d7f49b302abe83654b10ad79
20CE043 <97497245+20CE043@users.noreply.github.com>
2022-01-11 11:09:20 +0530

    Add files via upload

---
+++ pratical_2_2_e.py
@@ -0,0 +1,7 @@
+#Name: Hena Kharwa
+#ID: 20CE043
+#Github link: https://github.com/20CE043/Practical-2-2.e.git
+#Aim: Write a Python program to find the length of a tuple
+t=(1,2,3,4,8,9)
+#using len function to find length of tuple
+print(len(t))
